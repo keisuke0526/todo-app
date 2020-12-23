fb993cb1670ea35a205ef07933487ae8541adbd6
keisuke0526 <f.keisuke0526@gmail.com>
2020-12-23 20:24:17 +0900

    edit Todo.scala

--- app/controllers/Home.scala
+++ app/controllers/Home.scala
@@ -14,12 +14,12 @@ import model.ViewValueHome
 @Singleton
 class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
 
-  def index() = Action { implicit req =>
-    val vv = ViewValueHome(
-      title  = "Home",
-      cssSrc = Seq("main.css"),
-      jsSrc  = Seq("main.js")
-    )
-    Ok(views.html.Home(vv))
-  }
+//  def index() = Action { implicit req =>
+//    val vv = ViewValueHome(
+//      title  = "Home",
+//      cssSrc = Seq("main.css"),
+//      jsSrc  = Seq("main.js")
+//    )
+//    Ok(views.html.Home(vv))
+//  }
 }
