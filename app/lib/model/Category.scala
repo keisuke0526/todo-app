efdf5fd3d9b5f17d4bdfda9556d929136ac3dfb1
keisuke0526 <f.keisuke0526@gmail.com>
2020-12-25 17:07:14 +0900

    create Category.scala

--- app/lib/model/Category.scala
+++ app/lib/model/Category.scala
@@ -1,30 +1,43 @@
-//package lib.model
-//
-//import ixias.model._
-//import ixias.util.EnumStatus
-//
-//import java.time.LocalDateTime
-//
-//import Category._
-//case class Category(
-//  id:         Option[Id],
-//  name:       String,
-//  slug:       String,
-//  color:      Color,
-//  updatedAt:  LocalDateTime = NOW,
-//  createdAt:  LocalDateTime = NOW
-//) extends EntityModel[Id]
-//
-//object Category{
-//
-//  val Id  = the[Identity[Id]]
-//  type Id = Long @@ Todo
-//  type WithNoId   = Entity.WithNoId [Id, Category]
-//  type EmbeddedId = Entity.EmbeddedId[Id, Category]
+package lib.model
 
-//メモ　カテゴリ設定されている色が反映がわからない
-//  sealed abstract class Color(val code: Short, val color: String) extends EnumStatus
-//  object Color extends EnumStatus.Of[Color] {
-//    case object Blue extends Color(code = 0, color = "")
-//  }
-//}
+import ixias.model._
+import ixias.util.EnumStatus
+
+import java.time.LocalDateTime
+
+import Category._
+case class Category(
+  id:         Option[Id],
+  name:       String,
+  slug:       String,
+  color:      CategoryColor,
+  updatedAt:  LocalDateTime = NOW,
+  createdAt:  LocalDateTime = NOW
+) extends EntityModel[Id]
+
+object Category{
+
+  val Id  = the[Identity[Id]]
+  type Id = Long @@ Category
+  type WithNoId   = Entity.WithNoId [Id, Category]
+  type EmbeddedId = Entity.EmbeddedId[Id, Category]
+
+ 
+  sealed abstract class CategoryColor(val code: Short) extends EnumStatus
+  object CategoryColor extends EnumStatus.Of[CategoryColor] {
+    case object BLUE extends CategoryColor(code = 0)
+    case object RED extends CategoryColor(code = 1)
+    case object YELLO extends CategoryColor(code = 2)
+  }
+
+  def build(name: String, slug: String, color: CategoryColor): WithNoId = {
+    new Entity.WithNoId(
+      new Category(
+        id    = None,
+        name  = name,
+        slug  = slug,
+        color = color
+      )
+    )
+  }
+}
